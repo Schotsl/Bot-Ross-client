1ff3acf45a42b2e3d5d58355e64597041de0d23c
Schotsl <sjorsvanholst@gmail.com>
2021-03-06 20:14:02 +0100

    🔥 Removed mobile support from nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse">
       <ul class="navbar-nav">
         <li class="nav-item">
           <router-link class="nav-link" to="/habits">Habits</router-link>
